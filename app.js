77020c6bc0270d26d8e1ff2a0b399ab1fd0508fb
sushrutmech <sushrutmech@gmail.com>
2023-01-31 11:09:40 +0530

    2nd commit

--- app.js
+++ app.js
@@ -10,18 +10,14 @@ const manageCourseRouter = require("./api/route/manageCourse");
 const commentsRouter = require("./api/route/comment");
 var cors = require('cors');
 
-
-var url = "mongodb://localhost:27017/mydb2";
-
-mongoose.connect(url);
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb+srv://sushrutmahajan:Sushrut123@cluster0.2wvkj.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 //console.log(db.on('error', console.error.bind(console, 'connection error:')))
 db.once('open', () => {
   console.log('DB connected...!!!!!!!!!');
 });
-
-
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,8 +28,8 @@ router.get("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "1800");
   res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-   });
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+});
 
 // app.use(function(req,res,next){
 //   res.header("Access-Control-Allow-Origin" ,'*');
@@ -42,19 +38,19 @@ router.get("/", (req, res) => {
 //   next();
 // })
 
-app.use('/main' , mainRouter);
-app.use('/auth' , authRouter);
-app.use('/users',manageUserRouter);
-app.use('/course',manageCourseRouter);
-app.use('/conversation',commentsRouter);
+app.use('/main', mainRouter);
+app.use('/auth', authRouter);
+app.use('/users', manageUserRouter);
+app.use('/course', manageCourseRouter);
+app.use('/conversation', commentsRouter);
 
-app.use('/' , (req , res ,next )=>{
-    
-    console.log('this controller is hitted')
-    res.status(404).json({
+app.use('/', (req, res, next) => {
 
-        error: 'page is not found 404 error and you hitted wrong url'
-    })
+  console.log('this controller is hitted')
+  res.status(404).json({
+
+    error: 'page is not found 404 error and you hitted wrong url'
+  })
 })
 
 
