8482ec4a9d8447a56fe61ab038ea9f3bf03209cf
sushrutmech <105488766+sushrutmech@users.noreply.github.com>
2023-03-16 09:02:24 +0530

    first commit

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const errorHandler = require('./middlewares/errorHandler');
 const server = http.createServer(app);
 
 app.use(errorHandler.errorHandler);
-server.listen(3000 , console.log('server is started at 3000'))
+server.listen(3007 , console.log('server is started at 30007'))
